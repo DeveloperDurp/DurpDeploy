847fe8bb0b1a85a1231189dbe2b66de81b48697a
Hermes <user@hermes-agent.durp.loc>
2026-09-12 13:09:44 -0500

    Fix template action alignment

--- internal/handler/mobile_variants_test.go
+++ internal/handler/mobile_variants_test.go
@@ -238,7 +238,7 @@ func TestMobile_RenderedHTML_preserves_disclosures_and_containment_when_authenti
 				),
 				`(?s)<div[^>]*id="templates-list"[^>]*>.*?<div[^>]*class="[^"]*overflow-x-auto[^"]*"[^>]*>\s*<table[^>]*class="[^"]*table[^"]*"`,
 				`(?s)data-template-action="edit"[^>]*href="/templates/[0-9]+/edit".*?data-template-action="delete".*?data-template-action="history"[^>]*href="/templates/[0-9]+/history"`,
-				`(?s)<th class="w-1/2 sm:w-48">Actions</th>`,
+				`(?s)<th class="w-1/2 text-left sm:w-48">Actions</th>`,
 			},
 			contents: []string{fixture.template.ScriptBody},
 		},
